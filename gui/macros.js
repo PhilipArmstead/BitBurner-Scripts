3132663b71e82f31819489fe7ab337b0c7124b55
philarmstead <philarmstead@gmail.com>
2022-01-04 20:03:11 +0000

    Adding group functionality to Macros

--- gui/macros.js
+++ gui/macros.js
@@ -1,34 +1,42 @@
 import VueApp from "/gui/lib/VueApp.js"
 import AppWindow from "/gui/component/Window.js"
+import UiButton from "/gui/component/UiButton.js"
 import { inputTerminalCommands } from "/gui/lib/terminal.js"
 import { macroList } from "/gui/config/macro-list.config.js"
 
 
-/** @param {NS} ns **/
-export async function main (ns) {
+export async function main () {
 	await VueApp.initialise()
-	const app = new VueApp(ns)
+	const app = new VueApp()
 	new AppWindow(app)
+	new UiButton(app)
 	app.mount({
 		template: `
 			<app-window title="Macros" :can-refresh="true" class="window--macros" @window:close="kill" @window:refresh="refresh">
 				<div class="macro-list">
-					<div v-for="[label, commands] in listItems" :key="label" class="macro">
-						<button class="macro__cta" @click="inputTerminalCommands(commands)">{{ label }}</button>
+					<div v-for="(lists, category) in items" :key="category" class="macro__group">
+						<h1 v-if="category !== 'uncategorised'" class="macro__title">{{ category }}</h1>
+						<div v-for="(commands, label) in lists" :key="label" class="macro">
+							<ui-button class="macro__cta" type="secondary" @click="inputTerminalCommands(commands)">{{ label }}</ui-button>
+						</div>
 					</div>
 				</div>
 			</app-window>
 		`,
 		setup () {
+			const items = {
+				uncategorised: Object.fromEntries(Object.entries(macroList).filter(([, list]) => Array.isArray(list)).map(([label, commands]) => [label, commands])),
+				...Object.fromEntries(Object.entries(macroList).filter(([, list]) => !Array.isArray(list)).map(([label, commands]) => [label, commands]))
+			}
+			console.log(items)
 			const kill = () => app.unmount()
 			const refresh = () => {
 				if (inputTerminalCommands(['home', 'run /gui/macros.js'])) {
 					kill()
 				}
 			}
-			const listItems = Object.entries(macroList).map(([label, commands]) => [label, commands])
 
-			return { listItems, inputTerminalCommands, kill, refresh }
+			return { inputTerminalCommands, items, kill, refresh }
 		},
 		style: `
 			.window--macros {
@@ -49,20 +57,17 @@ export async function main (ns) {
 
 				.macro {
 					margin: 0 15px 8px 3px;
-				}
 
-				.macro__cta {
-					background: none;
-					border: none;
-					border-radius: 2px;
-					box-shadow: 0 0 0px 1px #AAAAAA54;
-					color: inherit;
-					cursor: pointer;
-					padding: 6px 8px;
-					transition: box-shadow .2s linear;
+					&__group {
+						display: flex;
+						flex: 1 0 100%;
+						flex-wrap: wrap;
+					}
 
-					&:hover {
-						box-shadow: 0 0 0px 1px #AAA;
+					&__title {
+						color: #FFF;
+						flex: 1 0 100%;
+						font-size: 16px;
 					}
 				}
 			}
