a974507135aa86321d3da0aa43e1e4cbe217f9ec
philarmstead <philarmstead@gmail.com>
2022-01-04 20:04:29 +0000

    Comitting new macro config

--- gui/config/macro-list.config.js
+++ gui/config/macro-list.config.js
@@ -1,7 +1,9 @@
 export const macroList = {
-	"Buy all": ["buy BruteSSH.exe", "buy FTPCrack.exe", "buy relaySMTP.exe", "buy HTTPWorm.exe", "buy SQLInject.exe"],
 	"Root everything": ["home", "run /util/server/root-everything.js"],
-	"Buy servers": ["home", "run server-purchase.js --once"],
 	"Show servers": ["home", "run /gui/server-list.js"],
-	"Show processes": ["home", "run /gui/process-list.js"]
+	"Show processes": ["home", "run /gui/process-list.js"],
+	"Buy": {
+		"Buy hacks": ["buy BruteSSH.exe", "buy FTPCrack.exe", "buy relaySMTP.exe", "buy HTTPWorm.exe", "buy SQLInject.exe"],
+		"Buy servers": ["home", "run server-purchase.js --once"],
+	}
 }
