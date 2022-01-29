c5137e77285928001bd5ebb7fdd1212da18b2163
Phil <philarmstead@mailbox.org>
2022-01-29 11:11:11 +0000

    Made the shortcut QoL script Windows compatible and added some comments

--- qol/shortcuts.js
+++ qol/shortcuts.js
@@ -1,14 +1,33 @@
+/**
+ * Readme
+ * ------
+ * To configure the shortcut keys, modify the `shortcuts` variable below
+ * If you're on Windows, you can't use numbers and you have to match the key's `code` property
+ * If you're on Linux (and maybe MacOS) you can use anything, and either the `key` or `code` properties
+ * If you're in the browser, you can't use this at all
+ */
+
 /** @param {NS} ns **/
-export async function main (ns) {
-	// Ctrl + $key will trigger the shortcut
+export async function main(ns) {
+	const debugMode = false // Toggle this to see codes in the console
+
 	// Button indices are the clickable elements in the sidebar
+	// Windows
 	const shortcuts = [
-		{ key: '1', buttonIndex: 0 }, // Terminal
-		{ key: '2', buttonIndex: 1 }, // Editor
-		{ key: '3', buttonIndex: 2 }, // Active scripts
-		{ key: '4', buttonIndex: 4 }, // Stats
-		{ key: '5', buttonIndex: 5 }, // Factions
+		{ code: 'KeyT', buttonIndex: 0 }, // Terminal
+		{ code: 'KeyE', buttonIndex: 1 }, // Editor
+		{ code: 'BracketLeft', buttonIndex: 2 }, // Active scripts
+		{ code: 'BracketRight', buttonIndex: 4 }, // Stats
+		{ code: 'Backslash', buttonIndex: 5 }, // Factions
 	]
+	// Not Windows
+	// const shortcuts = [
+	// 	{ key: '1', buttonIndex: 0 }, // Terminal
+	// 	{ key: '2', buttonIndex: 1 }, // Editor
+	// 	{ key: '3'', buttonIndex: 2 }, // Active scripts
+	// 	{ key: '4', buttonIndex: 4 }, // Stats
+	// 	{ key: '5', buttonIndex: 5 }, // Factions
+	// ]
 
 	const win = globalThis
 	const body = win['document'].body
@@ -16,27 +35,33 @@ export async function main (ns) {
 
 	// Create global storage for callbacks
 	if (!win.qolShortcuts) {
-		win.qolShortcuts = {}
+		win.qolShortcuts = new Map()
 	}
 
-	shortcuts.forEach(({ key, buttonIndex }) => {
+	shortcuts.forEach((shortcut) => {
+		const { buttonIndex, code, key } = shortcut
+
 		// Remove callback associated with this key
-		if (win.qolShortcuts[key]) {
-			body.removeEventListener('keypress', win.qolShortcuts[key])
+		if (win.qolShortcuts.has(shortcut)) {
+			body.removeEventListener('keypress', win.qolShortcuts['get'](shortcut))
 		}
 
 		// Get button for this shortcut
 		const button = buttons[buttonIndex]
 		if (button) {
 			// Create click-handler
-			const callback = ({ key: pressedKey, ctrlKey }) => {
-				if (pressedKey === key && ctrlKey) {
+			const callback = (e) => {
+				if (debugMode) {
+					console.log(e)
+				}
+
+				if (e.ctrlKey && (e.key === key || e.code === code)) {
 					button[Object.keys(button)[1]].onClick()
 				}
 			}
 
 			// Store it to global storage and bind to event-listener
-			win.qolShortcuts[key] = callback
+			win.qolShortcuts.set(shortcut, callback)
 			body.addEventListener('keypress', callback)
 		}
 	})
